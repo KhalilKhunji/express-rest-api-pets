bbc1133ceecbf5e45767d76ebdc2c59c1ed119f0
KhalilKhunji <khalilkhunji@gmail.com>
2024-08-11 12:20:23 +0300

    finished update and delete

--- controllers/pets.js
+++ controllers/pets.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res) => {
       const createdPet = await Pet.create(req.body);
       res.status(201).json(createdPet);
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ error: 'oops something went wrong' });
     }
 });
 
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
       const foundPets = await Pet.find();
       res.status(200).json(foundPets);
     } catch (error) {
-      res.status(500).json({ error: error.message }); // 500 Internal Server Error
+      res.status(500).json({ error: 'oops something went wrong' }); // 500 Internal Server Error
     }
 });
 
@@ -32,9 +32,45 @@ router.get('/:petId', async (req, res) => {
       if (res.statusCode === 404) {
         res.json({ error: error.message });
       } else {
+        res.status(500).json({ error: 'oops something went wrong' });
+      }
+    }
+});
+
+router.delete('/:petId', async (req, res) => {
+    try {
+      const deletedPet = await Pet.findByIdAndDelete(req.params.petId);
+      if (!deletedPet) {
+        res.status(404);
+        throw new Error('This is a test error');
+      }
+    //   res.status(200).json({ id: deletedPet._id, status: 'deleted'});
+      res.status(204).json({});
+    } catch (error) {
+        if (res.statusCode === 404) {
+            res.json({ error: error.message });
+        } else {
+            res.status(500).json({ error: 'oops something went wrong' });
+        }
+    }
+});
+
+router.put('/:petId', async (req, res) => {
+    try {
+      const updatedPet = await Pet.findByIdAndUpdate(req.params.petId, req.body, {new: true});
+      if (!updatedPet) {
+        res.status(404);
+        throw new Error('Pet not found.');
+      };
+      res.status(200).json(updatedPet);
+    } catch (error) {
+      if (res.statusCode === 404) {
+        res.json({ error: error.message });
+      } else {
         res.status(500).json({ error: error.message });
       }
     }
 });
+  
 
 module.exports = router;
